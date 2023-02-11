2df4045fa586ce9c0a24248146d33923b4c9577d
DLitvinenko <dimaboblitvinenko@gmail.com>
2023-02-11 17:28:37 +0300

    Ex002+fix_Ex001

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -4,11 +4,15 @@ Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 if (a > b)
 {
-Console.WriteLine("Больше число: ");
-Console.Write(a);
+  Console.WriteLine("Больше число: ");
+  Console.Write(a);
+  Console.WriteLine("Меньшее число: ");
+  Console.Write(b);
 }
 else
 {
-Console.Write("Больше число: ");
-Console.Write(b);
+  Console.Write("Больше число: ");
+  Console.Write(b);
+  Console.WriteLine("Меньшее число: ");
+  Console.Write(a);
 }
